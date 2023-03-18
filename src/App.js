eb1acb8c097fd896eb9e2f4ef03b9cc2e7752b92
chandrasekharkondapaturi <chandu.mc87>
2023-03-18 13:25:10 +0000

    Nxt Trendz Protected Route

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {Switch, Route} from 'react-router-dom'
+
+import ProtectedRoute from './components/ProtectedRoute'
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
